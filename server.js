ab82837efb4e7679c3c3ac99f77667f430e69236
Mohammad111300 <mammadelec@gmail.com>
2024-01-09 12:43:24 +0330

    commit5

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: 'public'});
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
